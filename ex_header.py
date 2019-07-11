0bde35054fb7ba2918cfa0ed49f8837fed12d11e
Webb <donovan.webb@diamond.ac.uk>
2019-07-11 15:48:32 +0100

    Detects detector dimensions

--- ex_header.py
+++ ex_header.py
@@ -1,12 +1,10 @@
 import mrcfile as mrc
 
-# Non Linear
-infile = "/dls/ebic/data/staff-scratch/Yuriy/V1Vo/nr18477-51/supervisor_20180330_005922_epu_d30carb_gr2/Images-Disc1/GridSquare_29289631/Data/FoilHole_30242671_Data_30241736_30241737_20180330_0440.mrc"
-# infile = '/dls/ebic/data/staff-scratch/Yuriy/V1Vo/nr18477-51/raw/GridSquare_25123228/Data/FoilHole_25130916_Data_25128460_25128461_20180325_2342_Fractions.mrc' # Does not work, stack of images
-# Linear
-# infile = '/dls/ebic/data/staff-scratch/Yuriy/V1Vo/m03cm22937-2/supervisor_20190502_212658_V1Vo_EPUtest/Images-Disc1/GridSquare_1443044/FoilHoles/FoilHole_1468945_20190503_082746.mrc'
-# infile = '/dls/ebic/data/staff-scratch/Yuriy/V1Vo/m03cm22937-2/supervisor_20190502_212658_V1Vo_EPUtest/Images-Disc1/GridSquare_1443044/FoilHoles/FoilHole_1469210_20190503_100156.mrc'
-# infile = '/dls/ebic/data/staff-scratch/Yuriy/V1Vo/m03cm22937-2/supervisor_20190502_212658_V1Vo_EPUtest/Images-Disc1/GridSquare_1443044/GridSquare_20190502_225804.mrc'
+# infile = '/dls/ebic/data/staff-scratch/Yuriy/DataFileTypes/m02K2EPU_Mrc/em16619-23/GridSquare_9897066/FoilHole_9907179_Data_9923659_9923661_20190711_122055.mrc' # K2 Non-linear
+infile = "/dls/ebic/data/staff-scratch/Yuriy/DataFileTypes/m06K3EPU_Mrc/em20287-23/FoilHole_7821234_Data_7825668_7825670_20190708_1608.mrc"  # k3 Non-linear
+# framefile = '/dls/ebic/data/staff-scratch/Yuriy/DataFileTypes/m02K2EPU_Mrc/em16619-23/GridSquare_9897066/FoilHole_9907179_Data_9923659_9923661_20190711_122055-376198_frames.mrc' # K2 Non-linear frames
+# framefile = '/dls/ebic/data/staff-scratch/Yuriy/DataFileTypes/m06K3EPU_Mrc/em20287-23/FoilHole_7821234_Data_7825668_7825670_20190708_1608_fractions.mrc' # Does not work
+
 
 labels = {
     "Microscope Type": 4,
@@ -29,9 +27,9 @@ labels = {
 metadata = {}
 
 
-with mrc.open(infile) as mrc:
-    doc_small = mrc.header
-    doc = mrc.extended_header
+with mrc.open(infile) as data:
+    doc_small = data.header
+    doc = data.extended_header
 
 for category, x in labels.items():
     print category, ":", doc[0][x]
@@ -44,6 +42,17 @@ print doc[0][19], "<----- What is this?"
 # 	print x
 
 if metadata["Direct Detector Electron Counting"]:
-    print "\nNot in linear mode"
+    linMod = "Not in linear mode"
 else:
-    print "\nIn linear mode"
+    linMod = "\nIn linear mode"
+
+if (
+    metadata["nx"] * metadata["ny"] == 14238980 or 28477960
+):  # Two values for super resolution
+    detect = "K2"
+elif metadata["nx"] * metadata["ny"] == 23569920 or 47139840:
+    detect = "K3"
+else:
+    detect = "Unsure of detector"
+
+print detect, linMod
--- tiffs.py
+++ tiffs.py
@@ -1,8 +1,9 @@
 from PIL import Image
 
-with Image.open(
-    "/dls/ebic/data/staff-scratch/Donovan/relionTutor2/relion30_tutorial/Movies/20170629_00021_frameImage.tiff"
-) as tiff:
+# infile = '/dls/ebic/data/staff-scratch/Donovan/relionTutor2/relion30_tutorial/Movies/20170629_00021_frameImage.tiff'
+# infile = '/dls/ebic/data/staff-scratch/Yuriy/DataFileTypes/m06K3EPUcompTiff/em20287-23/FoilHole_7832439_Data_7825668_7825670_20190709_2345_fractions.tiff' #Compressed
+infile = "/dls/ebic/data/staff-scratch/Yuriy/DataFileTypes/m06K3EPUcompTiff/em20287-23/FoilHole_7832439_Data_7825668_7825670_20190709_2345.tiff"
+with Image.open(infile) as tiff:
     print ("Dimensions = {}".format(tiff.size))
     header = tiff.tag
 
