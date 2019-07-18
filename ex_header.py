f4bf23cb2c61750318cc126e3f46b5f6f9b90a08
Webb <donovan.webb@diamond.ac.uk>
2019-07-18 14:35:13 +0100

    Fixed super res image by just extracting header from doc and not data

--- ex_header.py
+++ ex_header.py
@@ -6,14 +6,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help="Input file name")
 args = parser.parse_args()
 
-# infile = '/dls/ebic/data/staff-scratch/Yuriy/DataFileTypes/m02K2EPU_Mrc/em16619-23/GridSquare_9897066/FoilHole_9907179_Data_9923659_9923661_20190711_122055.mrc' # K2 Non-linear
+
+"""A range of testing files."""
+infile = "/dls/ebic/data/staff-scratch/Yuriy/DataFileTypes/m02K2EPU_Mrc/em16619-23/GridSquare_9897066/FoilHole_9907179_Data_9923659_9923661_20190711_122055.mrc"  # K2 Non-linear
 # infile = '/dls/ebic/data/staff-scratch/Yuriy/DataFileTypes/m06K3EPU_Mrc/em20287-23/FoilHole_7821234_Data_7825668_7825670_20190708_1608.mrc' # k3 Non-linear
 # infile = '/dls/ebic/data/staff-scratch/Yuriy/DataFileTypes/m02K2EPU_Mrc/em16619-23/GridSquare_9897066/FoilHole_9907179_Data_9923659_9923661_20190711_122055-376198_frames.mrc' # K2 Non-linear frames
-# infile = '/dls/ebic/data/staff-scratch/Yuriy/DataFileTypes/m06K3EPU_Mrc/em20287-23/FoilHole_7821234_Data_7825668_7825670_20190708_1608_fractions.mrc' # Does not work
+# infile = '/dls/ebic/data/staff-scratch/Yuriy/DataFileTypes/m06K3EPU_Mrc/em20287-23/FoilHole_7821234_Data_7825668_7825670_20190708_1608_fractions.mrc' # K3 Super resolution
 
-infile = args.input
+# infile = args.input
 
 
+"""Data to be extracted from extended header of MRC file."""
 labels = {
     "Microscope Type": 4,
     "HT": 8,
@@ -34,6 +37,12 @@ labels = {
 }
 metadata = {}
 
+"""Camera dimensions (x*y) for normal and super resolution."""
+K2_dim = 14238980
+K2_dim_super = K2_dim * 4
+K3_dim = 23569920
+K3_dim_super = K3_dim * 4
+
 
 def metadata_mrc(doc, labels):
     for category, x in labels.items():
@@ -43,50 +52,59 @@ def metadata_mrc(doc, labels):
     # Unknown Value
     print doc[0][19], "<----- What is this?"
 
-    # for x in enumerate(doc):
-    # 	print x
-
     if metadata["Direct Detector Electron Counting"]:
         linMod = "Not in linear mode"
     else:
-        linMod = "\nIn linear mode"
+        linMod = "In linear mode"
 
-    if (
-        metadata["nx"] * metadata["ny"] == 14238980
-        or metadata["nx"] * metadata["ny"] == 28477960
-    ):  # Two values for super resolution
+    if metadata["nx"] * metadata["ny"] == K2_dim:
         detect = "K2"
-        print metadata["nx"] * metadata["ny"]
-    elif (
-        metadata["nx"] * metadata["ny"] == 23569920
-        or metadata["nx"] * metadata["ny"] == 47139840
-    ):
+        res = ""
+    elif metadata["nx"] * metadata["ny"] == K2_dim_super:
+        detect = "K2"
+        res = "Super Resolution"
+    elif metadata["nx"] * metadata["ny"] == K3_dim:
         detect = "K3"
+        res = ""
+    elif metadata["nx"] * metadata["ny"] == K3_dim_super:
+        detect = "K3"
+        res = "Super Resolution"
     else:
         detect = "Unsure of detector"
+        res = ""
 
-    print detect, linMod
+    print detect, linMod, res
 
 
-def metadata_frames(doc_all, labels):
-    nx = doc_all.shape[0]
-    ny = doc_all.shape[1]
+def metadata_frames(doc_header, labels):
+    nx = doc_header.nx
+    ny = doc_header.ny
 
-    if nx * ny == 14238980 or 28477960:  # Two values for super resolution
+    if nx * ny == K2_dim:
         detect = "K2"
-    elif nx * ny == 23569920 or 47139840:
+        res = ""
+    elif nx * ny == K2_dim_super:
+        detect = "K2"
+        res = "Super Resolution"
+    elif nx * ny == K3_dim:
         detect = "K3"
+        res = ""
+    elif nx * ny == K3_dim_super:
+        detect = "K3"
+        res = "Super Resolution"
     else:
         detect = "Unsure of detector"
+        res = ""
 
-    print detect
+    print detect, res
 
 
-with mrc.open(infile) as data:
-    doc_all = data.data
-    doc = data.extended_header
+# Main
+with mrc.open(infile, header_only=True) as f:
+    doc_header = f.header
+    doc = f.extended_header
 
 if doc.shape == (0,):
-    metadata_frames(doc_all, labels)
+    metadata_frames(doc_header, labels)
 else:
     metadata_mrc(doc, labels)
