1ad6edf31f175381f54746558e7528ae8f43eb95
Webb <donovan.webb@diamond.ac.uk>
2019-11-01 14:22:35 +0000

    changed names, noted where clusters are and added mcorr batches in other args!

---
+++ Cryolo_relion3.0/CryoloExternalJob.py
@@ -0,0 +1,162 @@
+#!/dls_sw/apps/python/anaconda/4.6.14/64/envs/cryolo/bin/python
+"""
+External job for calling cryolo within Relion 3.0
+in_mics are the micrographs to be picked on
+in_model is the model to be used, empty will use general model!
+
+Run in main Relion project directory
+external_cryolo.py --o $PATH_WHERE_TO_STORE --in_mics $PATH_TO_MCORR/CTF_STARFILE --box_size $BOX_SIZE --threshold 0.3 (optional: --in_model $PATH_TO_MODEL)
+eg:
+CryoloExternalJob.py --o "External" --in_mics "CtfFind/job004/micrographs_ctf.star" --box_size 300 --threshold 0.3
+"""
+
+import argparse
+import json
+import os
+import os.path
+import random
+import sys
+import shutil
+import pathlib
+import time
+
+import gemmi
+
+import CorrectPath
+
+##### SPECIFIC TO FACILITY ######
+# cluster submit script
+qsub_file = "/dls_sw/apps/EM/relion_cryolo/CryoloRelion-master/qsub.sh"
+gen_model = "/dls_sw/apps/EM/crYOLO/cryo_phosaurus/gmodel_phosnet_20190516.h5"
+conf_file = "/dls_sw/apps/EM/crYOLO/cryo_phosaurus/config.json"
+#################################
+
+
+def run_job(project_dir, job_dir, args_list):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--in_mics", help="Input micrographs STAR file")
+    parser.add_argument(
+        "--j", dest="threads", help="Number of threads to run (ignored)"
+    )
+    parser.add_argument("--box_size", help="Size of box (~ particle size)")
+    parser.add_argument("--threshold", help="Threshold for picking (default = 0.3)")
+    parser.add_argument("--in_model", help="model from previous job")
+    args = parser.parse_args(args_list)
+    thresh = args.threshold
+    box_size = args.box_size
+
+    if args.in_model is None:
+        model = gen_model
+    else:
+        if os.path.exists(os.path.join(project_dir, args.in_model)):
+            model = os.path.join(project_dir, args.in_model)
+        else:
+            print(" RELION_IT: Cannot find fine tuned model")
+
+    # Making a cryolo config file with the correct box size
+    with open(conf_file, "r") as json_file:
+        data = json.load(json_file)
+        data["model"]["anchors"] = [int(box_size), int(box_size)]
+    with open("config.json", "w") as outfile:
+        json.dump(data, outfile)
+
+    # Reading the micrographs star file from relion
+    in_doc = gemmi.cif.read_file(os.path.join(project_dir, args.in_mics))
+    data_as_dict = json.loads(in_doc.as_json())["#"]
+
+    try:
+        os.mkdir("cryolo_input")
+    except FileExistsError:
+        # Not crucial so if fails due to any reason just carry on
+        try:
+            with open("done_mics.txt", "a+") as f:
+                for micrograph in os.listdir("cryolo_input"):
+                    f.write(micrograph + "\n")
+        except:
+            pass
+        shutil.rmtree("cryolo_input")
+        os.mkdir("cryolo_input")
+
+    # Arranging files for cryolo to predict particles from
+    # Not crucial so if fails due to any reason just carry on
+    try:
+        with open("done_mics.txt", "r") as f:
+            done_mics = f.read().splitlines()
+    except:
+        done_mics = []
+    for micrograph in data_as_dict["_rlnmicrographname"]:
+        if os.path.split(micrograph)[-1] not in done_mics:
+            os.link(
+                os.path.join(project_dir, micrograph),
+                os.path.join(
+                    project_dir, job_dir, "cryolo_input", os.path.split(micrograph)[-1]
+                ),
+            )
+
+    print(" RELION_IT: Running from model {}".format(model))
+
+    ###CLUSTER
+    os.system(
+        f"{qsub_file} cryolo_predict.py -c config.json -i {os.path.join(project_dir, job_dir, 'cryolo_input')} -o {os.path.join(project_dir, job_dir, 'gen_pick')} -w {model} -g 0 -t {thresh}"
+    )
+    ### WAIT FOR DONEFILE! ###
+    while not os.path.exists(".cry_predict_done"):
+        time.sleep(1)
+    os.remove(".cry_predict_done")
+    ###CLUSTER
+
+    try:
+        os.mkdir("picked_stars")
+    except FileExistsError:
+        pass
+
+    # Arranging files for Relion to use
+    for picked in os.listdir(os.path.join(project_dir, job_dir, "gen_pick", "STAR")):
+        new_name = os.path.splitext(picked)[0] + "_manualpick" + ".star"
+        try:
+            os.link(
+                os.path.join(project_dir, job_dir, "gen_pick", "STAR", picked),
+                os.path.join(project_dir, job_dir, "picked_stars", new_name),
+            )
+        except:
+            pass
+
+    # Writing a star file for Relion
+    part_doc = open("_manualpick.star", "w")
+    part_doc.write(os.path.join(project_dir, args.in_mics))
+    part_doc.close()
+
+    # Required star file
+    out_doc = gemmi.cif.Document()
+    output_nodes_block = out_doc.add_new_block("output_nodes")
+    loop = output_nodes_block.init_loop(
+        "", ["_rlnPipeLineNodeName", "_rlnPipeLineNodeType"]
+    )
+    loop.add_row([os.path.join(job_dir, "_manualpick.star"), "2"])
+    out_doc.write_file("RELION_OUTPUT_NODES.star")
+    ctf_star = os.path.join(project_dir, args.in_mics)
+    CorrectPath.correct(ctf_star)
+
+
+def main():
+    """Change to the job working directory, then call run_job()"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--o", dest="out_dir", help="Output directory name")
+    known_args, other_args = parser.parse_known_args()
+    project_dir = os.getcwd()
+    try:
+        os.mkdir(known_args.out_dir)
+    except FileExistsError:
+        pass
+    os.chdir(known_args.out_dir)
+    try:
+        run_job(project_dir, known_args.out_dir, other_args)
+    except:
+        open("RELION_JOB_EXIT_FAILURE", "w").close()
+        raise
+    else:
+        open("RELION_JOB_EXIT_SUCCESS", "w").close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Cryolo_relion3.0/CryoloPipeline.py
@@ -0,0 +1,258 @@
+#!/dls_sw/apps/python/anaconda/4.6.14/64/envs/cryolo/bin/python
+"""
+This is the cryolo preprocessing pipeline run from relion_it script. This script first executes the relion_it pipeline up to picking - cryolo then runs through CryoloExternalJob.py and its output is used by relion extraction. All executions of this script after the first run in the background and in parallel to the relion_it script.
+
+As with the relion_it script, if RUNNING_RELION_IT is deleted then this script will stop.
+"""
+import os
+import time
+import argparse
+import ast
+import sys
+import runpy
+import subprocess
+import shutil
+
+import cryolo_relion_it
+
+##### SPECIFIC TO FACILITY ######
+cryolo_relion_directory = "/dls_sw/apps/EM/relion_cryolo/CryoloRelion-master/"
+# cryolo_relion_directory = '/home/yig62234/Documents/pythonEM/Cryolo_relion3.0'
+#################################
+
+
+def main():
+    # When this script is run in the background a few arguments and options need to be parsed
+    OPTIONS_FILE = "relion_it_options.py"
+    opts = cryolo_relion_it.RelionItOptions()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_repeats")
+    parser.add_argument("--runjobs")
+    parser.add_argument("--motioncorr_job")
+    parser.add_argument("--ctffind_job")
+    parser.add_argument("--ipass")
+    parser.add_argument("--user_opt_files")
+    parser.add_argument("--gui")
+    parser.add_argument("--manpick_job")
+    args = parser.parse_args()
+    num_repeats = int(args.num_repeats)
+    runjobs = ast.literal_eval(args.runjobs)
+    motioncorr_job = args.motioncorr_job
+    ctffind_job = args.ctffind_job
+    ipass = int(args.ipass)
+    gui = int(args.gui)
+    manpick_job = args.manpick_job
+
+    option_files = ast.literal_eval(args.user_opt_files)
+    if gui == 1:
+        option_files.append(OPTIONS_FILE)
+
+    for user_opt_file in option_files:
+        user_opts = runpy.run_path(user_opt_file)
+        opts.update_from(user_opts)
+
+    # Cluster?
+    queue_options = [
+        "Submit to queue? == Yes",
+        "Queue name:  == {}".format(opts.queue_name),
+        "Queue submit command: == {}".format(opts.queue_submit_command),
+        "Standard submission script: == {}".format(opts.queue_submission_template),
+        "Minimum dedicated cores per node: == {}".format(opts.queue_minimum_dedicated),
+    ]
+
+    RunJobsCry(
+        num_repeats,
+        runjobs,
+        motioncorr_job,
+        ctffind_job,
+        opts,
+        ipass,
+        queue_options,
+        manpick_job,
+    )
+
+
+def RunJobsCry(
+    num_repeats,
+    runjobs,
+    motioncorr_job,
+    ctffind_job,
+    opts,
+    ipass,
+    queue_options,
+    manpick_job,
+):
+    """
+    Very similar to relion_it preprocessing pipeline with the autopicker. Extract and select jobs are identical.
+    """
+    # Constants
+    PIPELINE_STAR = "default_pipeline.star"
+    RUNNING_FILE = "RUNNING_RELION_IT"
+    SECONDPASS_REF3D_FILE = "RELION_IT_2NDPASS_3DREF"
+    SETUP_CHECK_FILE = "RELION_IT_SUBMITTED_JOBS"
+    PREPROCESS_SCHEDULE_PASS1 = "PREPROCESS"
+    PREPROCESS_SCHEDULE_PASS2 = "PREPROCESS_PASS2"
+
+    for i in range(0, num_repeats):
+        if not os.path.exists("RUNNING_RELION_IT"):
+            print("Exiting cryolo pipeline")
+            exit
+        preprocess_schedule_name = "BEFORE_CRYOLO"
+        # Running jobs up until picking
+        cryolo_relion_it.RunJobs(runjobs, 1, 1, preprocess_schedule_name)
+        cryolo_relion_it.WaitForJob(motioncorr_job, 15)
+        cryolo_relion_it.WaitForJob(ctffind_job, 15)
+        cryolo_options = [
+            "--in_mics {}".format(os.path.join(ctffind_job + "micrographs_ctf.star")),
+            "--o {}".format("External"),
+            "--box_size {}".format(opts.extract_boxsize),
+            "--threshold {}".format(opts.cryolo_threshold),
+        ]
+        option_string = ""
+        for cry_option in cryolo_options:
+            option_string += cry_option
+            option_string += " "
+        if os.path.exists("ExternalFine/DONE"):
+            option_string += "--in_model 'ExternalFine/model.h5'"
+        command = (
+            os.path.join(cryolo_relion_directory, "CryoloExternalJob.py")
+            + " "
+            + option_string
+        )
+        print(" RELION_IT: RUNNING {}".format(command))
+        os.system(command)
+
+        if not os.path.exists("RUNNING_RELION_IT"):
+            print("Exiting cryolo pipeline")
+            exit
+
+        #### Set up manual pick job
+        if num_repeats == 1:
+            # In order to visualise cry picked particles
+            manpick_options = [
+                "Input micrographs: == {}micrographs_ctf.star".format(ctffind_job)
+            ]
+            manualpick_job_name = "cryolo_picks"
+            manualpick_alias = "cryolo_picks"
+            manpick_job, already_had_it = cryolo_relion_it.addJob(
+                "ManualPick",
+                manualpick_job_name,
+                SETUP_CHECK_FILE,
+                manpick_options,
+                alias=manualpick_alias,
+            )
+            cryolo_relion_it.RunJobs([manpick_job], 1, 1, "ManualPick")
+
+        # wait for Manpick to make movies directory tree
+        wait_count = 0
+        # movies_dir to make sure if they named 'Movies' file differently it wont fail
+        movies_dir = opts.import_images.split("/")[0]
+        while not os.path.exists(manpick_job):
+            if wait_count > 15:
+                # but dont wait too long as not too important
+                break
+            time.sleep(2)
+            wait_count += 1
+
+        if wait_count <= 15:
+            try:
+                shutil.rmtree(os.path.join(manpick_job, movies_dir))
+                # Multiple reasons this could fail... Not crucial
+            except:
+                pass
+            shutil.copytree(
+                os.path.join("External", movies_dir),
+                os.path.join(manpick_job, movies_dir),
+            )
+
+        #### Set up the Extract job
+        extract_options = [
+            "Input coordinates:  == {}_manualpick.star".format("External/"),
+            "micrograph STAR file:  == {}micrographs_ctf.star".format(ctffind_job),
+            "Diameter background circle (pix):  == {}".format(opts.extract_bg_diameter),
+            "Particle box size (pix): == {}".format(
+                opts.extract_boxsize / opts.motioncor_binning
+            ),
+            "Number of MPI procs: == {}".format(opts.extract_mpi),
+        ]
+
+        if ipass == 0:
+            if opts.extract_downscale:
+                extract_options.append("Rescale particles? == Yes")
+                extract_options.append(
+                    "Re-scaled size (pixels):  == {}".format(opts.extract_small_boxsize)
+                )
+        else:
+            if opts.extract2_downscale:
+                extract_options.append("Rescale particles? == Yes")
+                extract_options.append(
+                    "Re-scaled size (pixels):  == {}".format(
+                        opts.extract2_small_boxsize
+                    )
+                )
+
+        if opts.extract_submit_to_queue:
+            extract_options.extend(queue_options)
+
+        if ipass == 0:
+            extract_job_name = "extract_job"
+            extract_alias = "pass 1"
+        else:
+            extract_job_name = "extract2_job"
+            extract_alias = "pass 2"
+
+        extract_job, already_had_it = cryolo_relion_it.addJob(
+            "Extract",
+            extract_job_name,
+            SETUP_CHECK_FILE,
+            extract_options,
+            alias=extract_alias,
+        )
+        secondjobs = [extract_job]
+
+        if (ipass == 0 and (opts.do_class2d or opts.do_class3d)) or (
+            ipass == 1 and (opts.do_class2d_pass2 or opts.do_class3d_pass2)
+        ):
+            #### Set up the Select job to split the particle STAR file into batches
+            split_options = [
+                "OR select from particles.star: == {}particles.star".format(
+                    extract_job
+                ),
+                "OR: split into subsets? == Yes",
+                "OR: number of subsets:  == -1",
+            ]
+
+            if ipass == 0:
+                split_job_name = "split_job"
+                split_options.append("Subset size:  == {}".format(opts.batch_size))
+                split_alias = "into {}".format(opts.batch_size)
+            else:
+                split_job_name = "split2_job"
+                split_options.append(
+                    "Subset size:  == {}".format(opts.batch_size_pass2)
+                )
+                split_alias = "into {}".format(opts.batch_size_pass2)
+
+            split_job, already_had_it = cryolo_relion_it.addJob(
+                "Select",
+                split_job_name,
+                SETUP_CHECK_FILE,
+                split_options,
+                alias=split_alias,
+            )
+
+            # Now start running stuff
+            secondjobs.append(split_job)
+            # Now execute the entire preprocessing pipeliner
+            if ipass == 0:
+                preprocess_schedule_name = PREPROCESS_SCHEDULE_PASS1
+            else:
+                preprocess_schedule_name = PREPROCESS_SCHEDULE_PASS2
+            cryolo_relion_it.RunJobs(secondjobs, 1, 1, preprocess_schedule_name)
+    if num_repeats == 1:
+        return split_job, manpick_job
+
+
+if __name__ == "__main__":
+    main()
