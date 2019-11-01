b97c0671e17d6527e58b273e55d8193e682ab541
Webb <donovan.webb@diamond.ac.uk>
2019-11-01 11:20:23 +0000

    Added conf file

---
+++ Cryolo_relion3.0/relion_it_config.py
@@ -0,0 +1,15 @@
+"""
+Options needed for making cryolo_relion_it facility specific
+"""
+
+# Location of the cryolo_relion_it files
+cryolo_relion_directory = ""
+
+# Location of the cryolo specific files
+cryolo_config = ""
+cryolo_gmodel = ""
+
+# Cluster options
+use_cluster = True
+qsub_file = ""
+qtemplate_file = ""
