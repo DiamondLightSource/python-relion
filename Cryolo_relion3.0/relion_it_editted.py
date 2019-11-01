572ff0067570dc85835875e78636894f30d1dff6
Webb <donovan.webb@diamond.ac.uk>
2019-11-01 10:59:32 +0000

    removed industrial group permissions

--- Cryolo_relion3.0/relion_it_editted.py
+++ Cryolo_relion3.0/relion_it_editted.py
@@ -979,22 +979,11 @@ class RelionItGui(object):
 
         row += 1
 
-        ### Checking if industrial user is trying to use relion_it...
-        not_allowed = ["m10_valid_users", "m10_staff", "m08_valid_users", "m08_staff"]
-        uid = os.getegid()
-        fedid = grp.getgrgid(uid)[0]
-        groups = str(subprocess.check_output(["groups", "{}".format(fedid)]))
-        if any(group in groups for group in not_allowed):
-            print("Industrial use of crYOLO is prohibited")
-            industrial_group = True
-        else:
-            industrial_group = False
-
         tk.Label(pipeline_frame, text="Use crYOLO?").grid(row=row, sticky=tk.W)
         self.use_cryolo_var = tk.IntVar()
         use_cryolo_button = tk.Checkbutton(pipeline_frame, var=self.use_cryolo_var)
         use_cryolo_button.grid(row=row, column=1, sticky=tk.W)
-        if options.autopick_do_cryolo and not industrial_group:
+        if options.autopick_do_cryolo:
             use_cryolo_button.select()
 
         row += 1
@@ -1190,9 +1179,8 @@ class RelionItGui(object):
             new_state = tk.DISABLED if self.stop_after_ctf_var.get() else tk.NORMAL
             class2d_button.config(state=new_state)
             class3d_button.config(state=new_state)
-            if not industrial_group:
-                use_cryolo_button.config(state=new_state)
-                cryolo_fine_button.config(state=new_state)
+            use_cryolo_button.config(state=new_state)
+            cryolo_fine_button.config(state=new_state)
             self.particle_max_diam_entry.config(state=new_state)
             self.particle_min_diam_entry.config(state=new_state)
             self.ref_3d_entry.config(state=new_state)
@@ -1249,10 +1237,6 @@ class RelionItGui(object):
             # Update the box size controls with care to avoid activating them when we shouldn't
             auto_boxsize_button.config(state=tk.DISABLED)
 
-        if industrial_group:
-            use_cryolo_button.config(state=tk.DISABLED)
-            cryolo_fine_button.config(state=tk.DISABLED)
-
         if not self.use_cryolo_var.get():
             cryolo_fine_button.config(state=tk.DISABLED)
         else:
@@ -1292,23 +1276,12 @@ class RelionItGui(object):
         Raises:
             ValueError: If an option value is invalid.
         """
-        ### Checking if industrial user is trying to use relion_it..
-        not_allowed = ["m10_valid_users", "m10_staff", "m08_valid_users", "m08_staff"]
-        uid = os.getegid()
-        fedid = grp.getgrgid(uid)[0]
-        groups = str(subprocess.check_output(["groups", "{}".format(fedid)]))
-        if any(group in groups for group in not_allowed):
-            industrial_group = True
-        else:
-            industrial_group = False
 
         opts = self.options
         warnings = []
 
         opts.stop_after_ctf_estimation = self.get_var_as_bool(self.stop_after_ctf_var)
         opts.autopick_do_cryolo = self.get_var_as_bool(self.use_cryolo_var)
-        if industrial_group:
-            opts.autopick_do_cryolo = False
         opts.do_class2d = self.get_var_as_bool(self.class2d_var)
         opts.do_class3d = self.get_var_as_bool(self.class3d_var)
         opts.do_second_pass = self.get_var_as_bool(self.second_pass_var)
