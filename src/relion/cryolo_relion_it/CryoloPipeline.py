39dfe6b9b5decd9188f898bf1d0a411926af0180
Webb <donovan.webb@diamond.ac.uk>
2019-11-15 10:54:22 +0000

    fixed mcorr binned extract box size to be even

--- src/relion/cryolo_relion_it/CryoloPipeline.py
+++ src/relion/cryolo_relion_it/CryoloPipeline.py
@@ -165,13 +165,13 @@ def RunJobsCry(
             )
 
         #### Set up the Extract job
+        bin_corrected_box_exact = int(opts.extract_boxsize / opts.motioncor_binning)
+        bin_corrected_box_even = bin_corrected_box_exact + bin_correct_box_exact % 2
         extract_options = [
             "Input coordinates:  == {}_manualpick.star".format("External/"),
             "micrograph STAR file:  == {}micrographs_ctf.star".format(ctffind_job),
             "Diameter background circle (pix):  == {}".format(opts.extract_bg_diameter),
-            "Particle box size (pix): == {}".format(
-                opts.extract_boxsize / opts.motioncor_binning
-            ),
+            "Particle box size (pix): == {}".format(bin_corrected_box_even),
             "Number of MPI procs: == {}".format(opts.extract_mpi),
         ]
 
