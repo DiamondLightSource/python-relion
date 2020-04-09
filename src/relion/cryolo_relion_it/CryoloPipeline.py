a604eb727e0c1c8ea6acdd99aa8721b7152fe098
Donovan <lexi@Lexi>
2020-04-09 14:11:26 +0100

    cwd

--- src/relion/cryolo_relion_it/CryoloPipeline.py
+++ src/relion/cryolo_relion_it/CryoloPipeline.py
@@ -115,7 +115,11 @@ def RunJobsCry(
             option_string += " "
         if os.path.exists("ExternalFine/DONE"):
             option_string += "--in_model 'ExternalFine/model.h5'"
-        print(os.getcwd())
+
+        import pathlib
+
+        print(pathlib.Path(__file__).parent.absolute())
+
         relion_pipeline_home = os.environ["RELION_PIPELINE_HOME"]
         external_path = os.path.join(relion_pipeline_home, "CryoloExternalJob.py")
         command = external_path + " " + option_string
