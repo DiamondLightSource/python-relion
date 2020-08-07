0b3e25f73ac34f56069488616978f9b35594e8a4
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-08-07 09:35:58 +0100

    Update author information

--- relion/__init__.py
+++ relion/__init__.py
@@ -4,7 +4,7 @@ https://github.com/DiamondLightSource/python-relion
 """
 
 __all__ = []
-__author__ = """Markus Gerstel"""
+__author__ = "Diamond Light Source - Scientific Software"
 __email__ = "scientificsoftware@diamond.ac.uk"
 __version__ = "0.0.1"
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
