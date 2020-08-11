a50b5981b137f39e9e1b474536229f5077c422e8
Anna Horstmann <anna.horstmann@diamond.ac.uk>
2020-08-11 11:18:27 +0100

    Reformat functions and use named tuple for the output

--- relion/MotionCorrection.py
+++ relion/MotionCorrection.py
@@ -1,6 +1,8 @@
 from gemmi import cif
 from pathlib import Path
 import os
+import functools
+from collections import namedtuple
 
 
 class MotionCorrection:
@@ -8,10 +10,11 @@ class MotionCorrection:
         self.relion_dir = data_dir
         self.directory = str(self.relion_dir)
         self.file_name = None
-        self.job_num = ""
+        # self.job_num = ""
         self.val_accum_motion_total = None
         self.val_accum_motion_early = None
         self.val_accum_motion_late = None
+        self.val_micrograph_name = None
 
     @property
     def accum_motion_total(self):
@@ -25,56 +28,72 @@ class MotionCorrection:
     def accum_motion_early(self):
         return self.val_accum_motion_early
 
+    @property
+    def micrograph_name(self):
+        return self.val_micrograph_name
+
     def set_total_accum_motion(self):
-        file_path = Path(self.directory) / "MotionCorr"
-        final_list = []
-        list = []
-        for x in file_path.iterdir():
-            if "job" in x.name:
-                self.job_num = x.name
-                AMT_list = self.parse_star_file("_rlnAccumMotionTotal", 1)
-                list = [self.job_num] + AMT_list
-            final_list.append(list)
-        self.val_accum_motion_total = final_list
+        values = self.find_values("_rlnAccumMotionTotal")
+        self.val_accum_motion_total = values
 
     def set_late_accum_motion(self):
-        file_path = Path(self.directory) / "MotionCorr"
-        final_list = []
-        list = []
-        for x in file_path.iterdir():
-            if "job" in x.name:
-                self.job_num = x.name
-                AML_list = self.parse_star_file("_rlnAccumMotionLate", 1)
-                list = [self.job_num] + AML_list
-            final_list.append(list)
-        self.val_accum_motion_late = final_list
+        values = self.find_values("_rlnAccumMotionLate")
+        self.val_accum_motion_late = values
 
     def set_early_accum_motion(self):
-        file_path = Path(self.directory) / "MotionCorr"
-        final_list = []
-        list = []
-        for x in file_path.iterdir():
-            if "job" in x.name:
-                self.job_num = x.name
-                AME_list = self.parse_star_file("_rlnAccumMotionEarly", 1)
-                list = [self.job_num] + AME_list
-            final_list.append(list)
-        self.val_accum_motion_early = final_list
+        values = self.find_values("_rlnAccumMotionEarly")
+        self.val_accum_motion_early = values
 
-    def parse_star_file(self, loop_name, block_number):
-        values_list = []
-        full_path = (
-            Path(self.directory)
-            / "MotionCorr"
-            / self.job_num
-            / "corrected_micrographs.star"
-        )
-        gemmi_readable_path = os.fspath(full_path)
-        star_doc = cif.read_file(gemmi_readable_path)
+    def set_micrograph_name(self):
+        values = self.find_values("_rlnMicrographName")
+        self.val_micrograph_name = values
+
+    def parse_star_file(self, loop_name, star_doc, block_number):
         data_block = star_doc[block_number]
         values = data_block.find_loop(loop_name)
-        for x in values:
-            values_list.append(x)
+        values_list = list(values)
         if not values_list:
             print("Warning - no values found for", loop_name)
         return values_list
+
+    def find_values(self, value):
+        file_path = Path(self.directory) / "MotionCorr"
+        final_list = []
+        for x in file_path.iterdir():
+            if "job" in x.name:
+                job = x.name
+                doc = self._read_star_file(job)
+                val_list = list(self.parse_star_file(value, doc, 1))
+                final_list.extend(val_list)
+        return final_list
+
+    @functools.lru_cache(maxsize=None)
+    def _read_star_file(self, job_num):
+        full_path = (
+            Path(self.directory) / "MotionCorr" / job_num / "corrected_micrographs.star"
+        )
+        gemmi_readable_path = os.fspath(full_path)
+        star_doc = cif.read_file(gemmi_readable_path)
+        return star_doc
+
+    def construct_dict(
+        self,
+        micrograph_name_list,
+        total_motion_list,
+        early_motion_list,
+        late_motion_list,
+    ):  # *args):
+        final_dict = {}
+        MCMicrographs = namedtuple(
+            "Micrograph", ["Total_motion", "Early_motion", "Late_motion"]
+        )
+        for i in range(len(micrograph_name_list)):
+            p = MCMicrographs(
+                total_motion_list[i], early_motion_list[i], late_motion_list[i]
+            )
+            final_dict[micrograph_name_list[i]] = {
+                p.Total_motion,
+                p.Early_motion,
+                p.Late_motion,
+            }
+        print(final_dict)
--- tests/test_MotionCorr.py
+++ tests/test_MotionCorr.py
@@ -11,27 +11,38 @@ def input_test_folder(dials_data):
 def test_total_value(input_test_folder):
     mc_object = MC.MotionCorrection(input_test_folder)
     mc_object.set_total_accum_motion()
-    print(mc_object)
-    total_motion = mc_object.accum_motion_total()
-    print(total_motion)
-    assert total_motion[0][1] == "16.420495"
+    total_motion = mc_object.accum_motion_total
+    assert total_motion[0] == "16.420495"
 
 
 def test_late_motion(input_test_folder):
     mc_object = MC.MotionCorrection(input_test_folder)
     mc_object.set_late_accum_motion()
-    late_motion = mc_object.accum_motion_late()
-    assert late_motion[0][1] == "13.914187"
+    late_motion = mc_object.accum_motion_late
+    assert late_motion[0] == "13.914187"
 
 
 def test_early_motion(input_test_folder):
     mc_object = MC.MotionCorrection(input_test_folder)
     mc_object.set_early_accum_motion()
-    early_motion = mc_object.accum_motion_early()
-    assert early_motion[0][1] == "2.506308"
+    early_motion = mc_object.accum_motion_early
+    assert early_motion[0] == "2.506308"
 
 
 def test_invalid_loop_name(input_test_folder):
     mc_object = MC.MotionCorrection(input_test_folder)
-    early_motion = mc_object.accum_motion_early()
+    early_motion = mc_object.accum_motion_early
     assert early_motion is None
+
+
+def test_dict(input_test_folder):
+    mc_object = MC.MotionCorrection(input_test_folder)
+    mc_object.set_early_accum_motion()
+    mc_object.set_late_accum_motion()
+    mc_object.set_total_accum_motion()
+    mc_object.set_micrograph_name()
+    early_motion = mc_object.accum_motion_early
+    late_motion = mc_object.accum_motion_late
+    total_motion = mc_object.accum_motion_total
+    names = mc_object.micrograph_name
+    mc_object.construct_dict(names, total_motion, early_motion, late_motion)
