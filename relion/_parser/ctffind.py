f2cd364701d203e26d2f32b496a7ae7f8870e7be
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-08-24 10:20:20 +0100

    remove misleading comment [skip ci]

--- relion/_parser/ctffind.py
+++ relion/_parser/ctffind.py
@@ -93,8 +93,6 @@ class CTFFind(collections.abc.Mapping):
         return self._find_values("")
 
     def _load_job_directory(self, jobdir):
-        # these are independent of jobdir, ie. this is a bug
-
         file = self._read_star_file(jobdir)
 
         astigmatism = self.parse_star_file("_rlnCtfAstigmatism", file, 1)
