ff0885bf3717359f0eae803aa66e8d324bb889ae
d-j-hatton <daniel.hatton@diamond.ac.uk>
2021-04-07 21:14:42 +0100

    Rewrite relion_it external job tests for pytest

--- tests/cryolo_relion_it/test_cryolo_external_job.py
+++ tests/cryolo_relion_it/test_cryolo_external_job.py
@@ -1,9 +1,6 @@
 import json
 import os
-import shutil
 import sys
-import tempfile
-import unittest
 from unittest import mock
 
 import gemmi
@@ -11,105 +8,103 @@ import gemmi
 from relion.cryolo_relion_it import cryolo_external_job
 
 
-class CryoloExternalJobTest(unittest.TestCase):
-    def setUp(self):
-        self.orig_dir = os.getcwd()
-        self.test_dir = tempfile.mkdtemp()
-        os.chdir(self.test_dir)
+@mock.patch("relion.cryolo_relion_it.cryolo_external_job.subprocess")
+def test_cryolo_external_job_main(mock_subprocess, tmpdir):
+    # Prepare things
+    os.chdir(tmpdir)
+    config_file = tmpdir.join("example_config.json")
+    with open(config_file, "w") as f:
+        json.dump({"model": {}}, f)
+    # with open(config_file, "w") as f:
+    #    json.dump({"model": {}}, f)
 
-    def tearDown(self):
-        os.chdir(self.orig_dir)
-        shutil.rmtree(self.test_dir)
+    mic_job_dir = "CtfFind/job001"
+    mic_job_dir = tmpdir.mkdir("CtfFind").mkdir("job001")
+    mic_dir = mic_job_dir.mkdir("Micrographs")
+    # os.makedirs(mic_dir)
+    mic_file = mic_dir.join("micrograph_1.mrc")
+    mic_file.write("")
+    # open(mic_file, "w").close()
 
-    @mock.patch("relion.cryolo_relion_it.cryolo_external_job.subprocess")
-    def test_main(self, mock_subprocess):
-        # Prepare things
-        config_file = "example_config.json"
-        with open(config_file, "w") as f:
-            json.dump({"model": {}}, f)
+    mic_star_file = mic_job_dir.join("example_mics.star")
+    mics_doc = gemmi.cif.Document()
+    block = mics_doc.add_new_block("micrographs")
+    loop = block.init_loop("_rln", ["MicrographName"])
+    loop.add_row(["CtfFind/job001/Micrographs/micrograph_1.mrc"])
+    mics_doc.write_file(str(mic_star_file))
 
-        mic_job_dir = "CtfFind/job001"
-        mic_dir = os.path.join(mic_job_dir, "Micrographs")
-        os.makedirs(mic_dir)
-        mic_file = os.path.join(mic_dir, "micrograph_1.mrc")
-        open(mic_file, "w").close()
+    job_dir = tmpdir.mkdir("External").mkdir("job002")
 
-        mic_star_file = os.path.join(mic_job_dir, "example_mics.star")
-        mics_doc = gemmi.cif.Document()
-        block = mics_doc.add_new_block("micrographs")
-        loop = block.init_loop("_rln", ["MicrographName"])
-        loop.add_row([mic_file])
-        mics_doc.write_file(mic_star_file)
+    cryolo_star_dir = job_dir.mkdir("gen_pick").mkdir("STAR")
+    # os.makedirs(cryolo_star_dir)
+    cryolo_file = cryolo_star_dir.join("micrograph_1.mrc")
+    cryolo_file.write("")
+    # open(os.path.join(cryolo_star_dir, os.path.basename(mic_file)), "w").close()
 
-        job_dir = "External/job002"
+    # Run the job
+    orig_sys_argv = sys.argv[:]
+    sys.argv[1:] = [
+        "--o",
+        "External/job002",
+        "--pipeline_control",
+        "External/job002",
+        "--in_mics",
+        "CtfFind/job001/example_mics.star",
+        "--box_size",
+        "256",
+        "--threshold",
+        "0.3",
+        "--gmodel",
+        "path/to/cryolo/gmodel.h5",
+        "--config",
+        str(tmpdir / "example_config.json"),
+        "--gpu",
+        '"0 1"',
+        "--j",
+        "10",
+    ]
+    cryolo_external_job.main()
 
-        cryolo_star_dir = os.path.join(job_dir, "gen_pick", "STAR")
-        os.makedirs(cryolo_star_dir)
-        open(os.path.join(cryolo_star_dir, os.path.basename(mic_file)), "w").close()
-
-        # Run the job
-        orig_sys_argv = sys.argv[:]
-        sys.argv[1:] = [
-            "--o",
-            job_dir,
-            "--pipeline_control",
-            job_dir,
-            "--in_mics",
-            mic_star_file,
-            "--box_size",
-            "256",
-            "--threshold",
-            "0.3",
-            "--gmodel",
+    # Check results
+    mock_subprocess.run.assert_called_once_with(
+        [
+            "cryolo_predict.py",
+            "--conf",
+            "config.json",
+            "-i",
+            f"{tmpdir}/External/job002/cryolo_input",
+            "-o",
+            f"{tmpdir}/External/job002/gen_pick",
+            "--weights",
             "path/to/cryolo/gmodel.h5",
-            "--config",
-            os.path.join(self.test_dir, config_file),
             "--gpu",
             '"0 1"',
-            "--j",
-            "10",
-        ]
-        cryolo_external_job.main()
+            "--threshold",
+            "0.3",
+        ],
+        check=True,
+    )
+    assert os.path.isfile(
+        tmpdir / "External" / "job002" / "coords_suffix_autopick.star"
+    )
+    with open(tmpdir / "External" / "job002" / "coords_suffix_autopick.star") as f:
+        contents = f.read().strip()
+        assert contents == "CtfFind/job001/example_mics.star"
 
-        # Check results
-        mock_subprocess.run.assert_called_once_with(
-            [
-                "cryolo_predict.py",
-                "--conf",
-                "config.json",
-                "-i",
-                f"{self.test_dir}/External/job002/cryolo_input",
-                "-o",
-                f"{self.test_dir}/External/job002/gen_pick",
-                "--weights",
-                "path/to/cryolo/gmodel.h5",
-                "--gpu",
-                '"0 1"',
-                "--threshold",
-                "0.3",
-            ],
-            check=True,
-        )
-        assert os.path.isfile("coords_suffix_autopick.star")
-        with open("coords_suffix_autopick.star") as f:
-            contents = f.read().strip()
-            assert contents == mic_star_file
+    assert os.path.isdir(tmpdir / "External" / "job002" / "picked_stars")
+    assert os.path.isfile(
+        tmpdir / "External" / "job002" / "Micrographs" / "micrograph_1_autopick.star"
+    )
+    assert len(os.listdir(tmpdir / "External" / "job002" / "picked_stars")) == 0
 
-        assert os.path.isdir("picked_stars")
-        assert os.path.isfile("Micrographs/micrograph_1_autopick.star")
-        assert len(os.listdir("picked_stars")) == 0
+    assert os.path.isfile(tmpdir / "External" / "job002" / "RELION_OUTPUT_NODES.star")
+    doc = gemmi.cif.read_file(
+        str(tmpdir / "External" / "job002" / "RELION_OUTPUT_NODES.star")
+    )
+    block = doc.sole_block()
+    table = block.find("_rlnPipeLineNode", ["Name", "Type"])
+    assert len(table) == 1
+    assert list(table[0]) == ["External/job002/coords_suffix_autopick.star", "2"]
 
-        assert os.path.isfile("RELION_OUTPUT_NODES.star")
-        doc = gemmi.cif.read_file("RELION_OUTPUT_NODES.star")
-        block = doc.sole_block()
-        table = block.find("_rlnPipeLineNode", ["Name", "Type"])
-        assert len(table) == 1
-        assert list(table[0]) == ["External/job002/coords_suffix_autopick.star", "2"]
-
-        # Restore state
-        os.chdir(self.test_dir)
-        sys.argv = orig_sys_argv
-
-
-if __name__ == "__main__":
-    unittest.main()
+    # Restore state
+    sys.argv = orig_sys_argv
--- tests/cryolo_relion_it/test_select_and_split_external_job.py
+++ tests/cryolo_relion_it/test_select_and_split_external_job.py
@@ -1,118 +1,98 @@
 import json
 import os
-import shutil
 import sys
-import tempfile
-import unittest
+from unittest import mock
 
 import gemmi
 
 from relion.cryolo_relion_it import select_and_split_external_job
 
 
-class SelectAndSplitExternalJobTest(unittest.TestCase):
-    def setUp(self):
-        self.orig_dir = os.getcwd()
-        self.test_dir = tempfile.mkdtemp()
-        os.chdir(self.test_dir)
+@mock.patch("relion.cryolo_relion_it.select_and_split_external_job.subprocess")
+def test_select_and_split_main(mock_subprocess, tmpdir):
+    # Prepare things
+    os.chdir(tmpdir)
+    config_file = tmpdir.join("example_config.json")
+    with open(config_file, "w") as f:
+        json.dump({"model": {}}, f)
 
-    def tearDown(self):
-        os.chdir(self.orig_dir)
-        shutil.rmtree(self.test_dir)
+    mic_job_dir = tmpdir.mkdir("CtfFind").mkdir("job001")
+    mic_dir = mic_job_dir.mkdir("Micrographs")
+    mic_file = mic_dir.join("micrograph_1.mrc")
+    mic_file.write("")
 
-    @unittest.mock.patch(
-        "relion.cryolo_relion_it.select_and_split_external_job.subprocess"
-    )
-    def test_main(self, mock_subprocess):
-        # Prepare things
-        config_file = "example_config.json"
-        with open(config_file, "w") as f:
-            json.dump({"model": {}}, f)
+    mic_star_file = mic_job_dir.join("example_mics.star")
+    mics_doc = gemmi.cif.Document()
+    block = mics_doc.add_new_block("micrographs")
+    loop = block.init_loop("_rln", ["MicrographName"])
+    loop.add_row([str(mic_file)])
+    mics_doc.write_file(str(mic_star_file))
 
-        mic_job_dir = "CtfFind/job001"
-        mic_dir = os.path.join(mic_job_dir, "Micrographs")
-        os.makedirs(mic_dir)
-        mic_file = os.path.join(mic_dir, "micrograph_1.mrc")
-        open(mic_file, "w").close()
+    ini_job_dir_name = "InitialModel/job002"
+    ini_job_dir = tmpdir.mkdir("InitialModel").mkdir("job002")
 
-        mic_star_file = os.path.join(mic_job_dir, "example_mics.star")
-        mics_doc = gemmi.cif.Document()
-        block = mics_doc.add_new_block("micrographs")
-        loop = block.init_loop("_rln", ["MicrographName"])
-        loop.add_row([mic_file])
-        mics_doc.write_file(mic_star_file)
+    ini_star_file = ini_job_dir.join("run_it300_data.star")
+    ini_doc = gemmi.cif.Document()
+    ini_block = ini_doc.add_new_block("data")
+    ini_loop = ini_block.init_loop("_rln", ["ClassNumber"])
+    ini_loop.add_row(["1"])
+    ini_loop_02 = ini_block.init_loop("_rln", ["MicrographName"])
+    ini_loop_02.add_row([str(mic_file)])
+    ini_doc.write_file(str(ini_star_file))
 
-        ini_job_dir = "InitialModel/job002"
-        os.makedirs(ini_job_dir)
+    job_dir = "External/job003"
 
-        ini_star_file = os.path.join(ini_job_dir, "run_it300_data.star")
-        ini_doc = gemmi.cif.Document()
-        ini_block = ini_doc.add_new_block("data")
-        ini_loop = ini_block.init_loop("_rln", ["ClassNumber"])
-        ini_loop.add_row(["1"])
-        ini_loop_02 = ini_block.init_loop("_rln", ["MicrographName"])
-        ini_loop_02.add_row([mic_file])
-        ini_doc.write_file(ini_star_file)
+    tmpdir.mkdir("External").mkdir("job003")
 
-        job_dir = "External/job003"
+    # Run the job
+    orig_sys_argv = sys.argv[:]
+    sys.argv[1:] = [
+        "--in_dir",
+        ini_job_dir_name,
+        "--out_dir",
+        job_dir,
+        "--in_mics",
+        str(ini_star_file),
+        "--outfile",
+        "particles_class1.star",
+        "--class_number",
+        "1",
+    ]
 
-        selsplit_star_dir = os.path.join(job_dir)
-        os.makedirs(selsplit_star_dir)
+    select_and_split_external_job.main()
 
-        # Run the job
-        orig_sys_argv = sys.argv[:]
-        sys.argv[1:] = [
-            "--in_dir",
-            ini_job_dir,
-            "--out_dir",
-            job_dir,
-            "--in_mics",
-            ini_star_file,
-            "--outfile",
-            "particles_class1.star",
-            "--class_number",
+    call1 = mock.call(
+        [
+            "relion_star_handler",
+            "--i",
+            str(ini_star_file),
+            "--o",
+            os.path.join(job_dir, "particles_class1.star"),
+            "--select",
+            "rlnClassNumber",
             "1",
-        ]
+        ],
+        check=True,
+    )
 
-        select_and_split_external_job.main()
+    call2 = mock.call(
+        [
+            "relion_star_handler",
+            "--i",
+            os.path.join(job_dir, "particles_class1.star"),
+            "--o",
+            os.path.join(job_dir, "particles_class1.star"),
+            "--split",
+            "--random_order",
+            "--nr_split",
+            "2",
+        ],
+        check=True,
+    )
 
-        call1 = unittest.mock.call(
-            [
-                "relion_star_handler",
-                "--i",
-                ini_star_file,
-                "--o",
-                os.path.join(job_dir, "particles_class1.star"),
-                "--select",
-                "rlnClassNumber",
-                "1",
-            ],
-            check=True,
-        )
+    mock_subprocess.run.assert_has_calls([call1, call2])
 
-        call2 = unittest.mock.call(
-            [
-                "relion_star_handler",
-                "--i",
-                os.path.join(job_dir, "particles_class1.star"),
-                "--o",
-                os.path.join(job_dir, "particles_class1.star"),
-                "--split",
-                "--random_order",
-                "--nr_split",
-                "2",
-            ],
-            check=True,
-        )
+    assert os.path.isfile(tmpdir / "External" / "job003" / "RELION_JOB_EXIT_SUCCESS")
 
-        mock_subprocess.run.assert_has_calls([call1, call2])
-
-        assert os.path.isfile("RELION_JOB_EXIT_SUCCESS")
-
-        # Restore state
-        os.chdir(self.test_dir)
-        sys.argv = orig_sys_argv
-
-
-if __name__ == "__main__":
-    unittest.main()
+    # Restore state
+    sys.argv = orig_sys_argv
