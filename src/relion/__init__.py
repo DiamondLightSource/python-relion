00866dcd5adc8bf3a69019d2321e7fd7afe323f2
d-j-hatton <daniel.hatton@diamond.ac.uk>
2021-04-14 10:18:08 +0100

    Bump version: 0.3.0 → 0.3.1

--- src/relion/__init__.py
+++ src/relion/__init__.py
@@ -15,7 +15,7 @@ import copy
 __all__ = []
 __author__ = "Diamond Light Source - Scientific Software"
 __email__ = "scientificsoftware@diamond.ac.uk"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
 
 
