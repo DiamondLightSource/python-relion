71f09a1d7df1a076980153df292277ec333046be
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-05-17 15:40:20 +0100

    Yet more Windows debugging

--- tests/test_project.py
+++ tests/test_project.py
@@ -138,6 +138,16 @@ def test_prepended_results_are_picked_up_correctly_by_fresh(dials_data, proj):
     fresh_results = [p for p in proj.results.fresh]
     print(proj.res._cache)
     print(proj.res._seen_before)
+    print(
+        (
+            dials_data("relion_tutorial_data", pathlib=True)
+            / "MotionCorr"
+            / "job002"
+            / "RELION_JOB_EXIT_SUCCESS"
+        )
+        .stat()
+        .st_ctime
+    )
     assert len(fresh_results[0][0]["job002"]) == 2
     assert (
         fresh_results[0][0]["job002"][0].micrograph_name
