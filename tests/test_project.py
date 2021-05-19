135099eb64aeb574d2c9db6a1e3174d9636bf951
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-05-19 14:09:34 +0100

    get_imported test fix

--- tests/test_project.py
+++ tests/test_project.py
@@ -56,9 +56,7 @@ def test_results_collection_does_not_crash_for_an_empty_project():
     assert results._results == []
 
 
-def test_get_imported_files_from_job_directory(dials_data):
-    imported = relion.get_imported(
-        dials_data("relion_tutorial_data", pathlib=True) / "Import" / "job001"
-    )
+def test_get_imported_files_from_job_directory(proj):
+    imported = proj.get_imported()
     assert len(imported) == 24
     assert imported[0] == "Movies/20170629_00021_frameImage.tiff"
