90c12c37fb8dec0b8ed9b4b021ffc60d59e90645
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-05-25 08:23:57 +0100

    FileNotFoundError should work in the future

--- src/relion/_parser/cryolo.py
+++ src/relion/_parser/cryolo.py
@@ -33,7 +33,7 @@ class Cryolo(JobType):
                 file = self._read_star_file(
                     jobdir, pathlib.Path("picked_stars") / star_file.name
                 )
-            except (RuntimeError, IOError):
+            except (RuntimeError, FileNotFoundError):
                 return []
 
             info_table = self._find_table_from_column_name("_rlnCoordinateX", file)
@@ -44,7 +44,7 @@ class Cryolo(JobType):
         # all of this just tracks back to a micrograph name from the MotionCorrection job
         try:
             jobfile = self._read_star_file(jobdir, "job.star")
-        except (RuntimeError, IOError):
+        except (RuntimeError, FileNotFoundError):
             return []
         info_table = self._find_table_from_column_name("_rlnJobOptionVariable", jobfile)
         variables = self.parse_star_file("_rlnJobOptionVariable", jobfile, info_table)
