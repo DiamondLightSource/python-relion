5c975b536cb1596c8dd56919fab1a0d2bef45ecb
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-07-31 15:22:48 +0100

    Add command line tool to launch pipeline within given directory (doesn't include any results collection)

---
+++ src/relion/cli/run_pipeline.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+import pathlib
+
+from relion.cryolo_relion_it import cryolo_relion_it, dls_options
+from relion.cryolo_relion_it.cryolo_relion_it import RelionItOptions
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        "--dir",
+        help="Working directory where Relion will run",
+        dest="working_directory",
+        default=".",
+    )
+    parser.add_argument(
+        "--param",
+        help="Set a key, value pair of options as key=value",
+        action="append",
+        dest="params",
+    )
+    args = parser.parse_args()
+
+    if args.params:
+        params_list = [p.split("=") for p in args.params]
+        params_dict = {p[0]: p[1] for p in params_list}
+        for k, v in params_dict.items():
+            if v.isnumeric():
+                params_dict[k] = int(v)
+            elif v.lower() == "true":
+                params_dict[k] = True
+            elif v.lower() == "false":
+                params_dict["ispyb_parameters"][k] = False
+            else:
+                try:
+                    params_dict["ispyb_parameters"][k] = float(v)
+                except ValueError:
+                    pass
+    else:
+        params_dict = {}
+
+    opts = RelionItOptions()
+    opts.update_from(vars(dls_options))
+    opts.update_from(params_dict)
+    options_file = pathlib.Path(args.working_directory) / cryolo_relion_it.OPTIONS_FILE
+    if os.path.isfile(options_file):
+        os.rename(options_file, f"{options_file}~")
+    with open(options_file, "w") as optfile:
+        opts.print_options(optfile)
+
+    os.chdir(args.working_directory)
+    cryolo_relion_it.run_pipeline(opts)
