2e9380c8888097fc5d555ce16ca20f41215ae923
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-08-05 18:29:14 +0100

    Bump version: 0.7.11 → 0.7.12

--- src/relion/__init__.py
+++ src/relion/__init__.py
@@ -34,7 +34,7 @@ logger = logging.getLogger("relion.Project")
 __all__ = []
 __author__ = "Diamond Light Source - Scientific Software"
 __email__ = "scientificsoftware@diamond.ac.uk"
-__version__ = "0.7.11"
+__version__ = "0.7.12"
 __version_tuple__ = tuple(int(x) for x in __version__.split("."))
 
 pipeline_lock = ".relion_lock"
