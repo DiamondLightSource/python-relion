406a959421e643031ceba0f818232e4b9dc78ee4
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-08-11 09:57:05 +0100

    Add Zocalo images service plugin method to make particle picker images with filtered background image and circles representing picked particles

--- src/relion/zocalo/images_service_plugin.py
+++ src/relion/zocalo/images_service_plugin.py
@@ -4,6 +4,7 @@ import pathlib
 import mrcfile
 import numpy as np
 import PIL.Image
+from PIL import ImageDraw, ImageEnhance, ImageFilter
 
 logger = logging.getLogger("relion.zocalo.images_service_plugin")
 
@@ -13,11 +14,11 @@ def mrc_to_jpeg(plugin_params):
     allframes = plugin_params.parameters("all_frames")
     if not filename or filename == "None":
         logger.debug("Skipping mrc to jpeg conversion: filename not specified")
-        return
+        return None
     filepath = pathlib.Path(filename)
     if not filepath.is_file():
         logger.error(f"File {filepath} not found")
-        return
+        return None
     try:
         with mrcfile.open(filepath) as mrc:
             data = mrc.data
@@ -48,4 +49,33 @@ def mrc_to_jpeg(plugin_params):
     logger.info(f"Converted mrc to jpeg {filename} -> {outfile}")
     if outfiles:
         return outfiles
+    return outfile
+
+
+def picked_particles(plugin_params):
+    basefilename = plugin_params.parameters("file")
+    coords = plugin_params.parameters("coordinates")
+    angpix = plugin_params.parameters("angpix")
+    diam = plugin_params.parameters("diameter")
+    contrast_factor = plugin_params.parameters("contrast_factor", default=6)
+    outfile = plugin_params.parameters("outfile")
+    if not outfile:
+        logger.warning(f"Outfile incorrectly specified: {outfile}")
+        return None
+    radius = (diam / angpix) // 2
+    try:
+        with PIL.Image.open(basefilename) as bim:
+            enhancer = ImageEnhance.Contrast(bim)
+            enhanced = enhancer.enhance(contrast_factor)
+            fim = enhanced.filter(ImageFilter.BLUR)
+            dim = ImageDraw.Draw(fim)
+            for x, y in coords:
+                dim.ellipse(
+                    [(x - radius, y - radius), (x + radius, y + radius)],
+                    width=8,
+                    outline="#f58a07",
+                )
+            dim.save(outfile)
+    except FileNotFoundError:
+        logger.error(f"File {basefilename} could not be opened")
     return outfile
