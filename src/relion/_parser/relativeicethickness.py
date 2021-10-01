079388271d2b8a681bbf261fbef01181e201ba1a
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2021-10-01 10:50:43 +0100

    Correct csv file name and labels produced by the csv parser

--- src/relion/_parser/relativeicethickness.py
+++ src/relion/_parser/relativeicethickness.py
@@ -43,15 +43,15 @@ class RelativeIceThickness(JobType):
 
     def _load_job_directory(self, jobdir):
         try:
-            ice_dict = self.csv_to_dict(self._basepath / jobdir / "five_figures.csv")
+            ice_dict = self.csv_to_dict(self._basepath / jobdir / "five_figs_test.csv")
         except (FileNotFoundError, OSError, RuntimeError, ValueError):
             return []
         list_micrograph_path = ice_dict["path"]
-        list_minimum = ice_dict["min"]
-        list_q1 = ice_dict["q1"]
-        list_median = ice_dict["median"]
-        list_q3 = ice_dict["q3"]
-        list_maximum = ice_dict["maximum"]
+        list_minimum = ice_dict[" min"]
+        list_q1 = ice_dict[" q1"]
+        list_median = ice_dict[" q2=median"]
+        list_q3 = ice_dict[" q3"]
+        list_maximum = ice_dict[" max"]
 
         micrograph_list = []
         for j in range(len(list_micrograph_path)):
