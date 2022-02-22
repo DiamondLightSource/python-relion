c27388bec6f538a849a04bf154bd6976754f4180
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-02-22 16:18:14 +0000

    Hope the ordering changes the outcome

--- src/relion/cryolo_relion_it/select_and_split_external_job.py
+++ src/relion/cryolo_relion_it/select_and_split_external_job.py
@@ -58,7 +58,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--in_dir", dest="in_dir", help="Input directory name")
     parser.add_argument(
-        "--out_dir", "--o", dest="out_dir", help="Output directory name"
+        "--o", "--out_dir", dest="out_dir", help="Output directory name"
     )
     parser.add_argument("--in_mics", dest="starin", help="Input star file name")
     parser.add_argument("--outfile", dest="starout", help="Output star file name")
