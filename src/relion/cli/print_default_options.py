922953241b42943a56230856f79882f7d54f2135
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-03-31 10:48:49 +0100

    Allow configuration of cluster qsub templates when printing default pipeline options

--- src/relion/cli/print_default_options.py
+++ src/relion/cli/print_default_options.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+
+import yaml
 
 from relion.cryolo_relion_it import dls_options
 from relion.cryolo_relion_it.cryolo_relion_it import RelionItOptions
@@ -16,4 +19,8 @@ def run():
     args = parser.parse_args()
     opts = RelionItOptions()
     opts.update_from(vars(dls_options))
+    if os.getenv("RELION_CLUSTER_CONFIG"):
+        with open(os.getenv("RELION_CLUSTER_CONFIG"), "r") as config:
+            cluster_config = yaml.safe_load(config)
+        opts.update_from(cluster_config)
     opts.print_options(out_file=open(args.options_file, "w"))
