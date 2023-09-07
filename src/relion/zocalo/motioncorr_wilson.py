2d9060c8ea60c6be1a5ba51eb5d9761705ac6a68
stephen-riggs <stephen.riggs@diamond.ac.uk>
2023-09-07 12:04:26 +0100

    Remove log files

--- src/relion/zocalo/motioncorr_wilson.py
+++ src/relion/zocalo/motioncorr_wilson.py
@@ -223,10 +223,9 @@ class MotionCorrWilson(MotionCorr, CommonService):
             slurm_job_state = "FAILED"
 
         if self.x_shift_list and self.y_shift_list and self.each_total_motion:
-            # Path(mc_output_file).unlink()
-            # Path(mc_error_file).unlink()
-            # Path(submission_file).unlink()
-            pass
+            Path(mc_output_file).unlink()
+            Path(mc_error_file).unlink()
+            Path(submission_file).unlink()
         else:
             self.log.error(f"Reading shifts from {mc_output_file} failed")
             slurm_job_state = "FAILED"
